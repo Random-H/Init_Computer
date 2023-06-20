4600d06b55aaeac64e3269eb8d900cf164f4ab09
Random-H <1092444593@qq.com>
2023-06-20 16:29:22 +0800

    2

---
+++ inc/env.h
@@ -0,0 +1,3 @@
+#include <iostream>
+using namespace std;
+int addEnvPath(char *path,string word,string envValue);
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "windows.h"
 #include <tchar.h>
 #include "../inc/reg.h"
+#include "../inc/env.h"
 using namespace std;
 
 
@@ -20,6 +21,7 @@ int main()
         switch (ch)
         {
         case '1':
+        {
             ret = HMD_CreateCMD();
             if(ret!=1)
             {
@@ -29,13 +31,41 @@ int main()
             {
                 cout<<"创建成功"<<endl;
             }
+        }
             break;
         case '2':
+        {
+            char buffer[256];
+            GetCurrentDirectoryA(256, buffer);
+            string folder;
+            string now_folder = string(buffer);
+            string file_folder = "\\tool\\MinGW64";
+            int re = now_folder.find("\\bin");
+            now_folder = now_folder.replace(re,file_folder.length(),file_folder);
+            cout<<"文件目录:"<<now_folder<<endl;
+            cout<<"输入MinGW64路径"<<endl;
+            cin>>folder;
+            cout<<"当前："<<now_folder<<endl;
+            cout<<"目标："<<folder<<endl;
+            string cmd = "xcopy "+now_folder+" "+folder+" /s/e/h/y";
+            system(cmd.c_str());
+            //xcopy "C:\Users\HMD\Desktop\Init\tool" "C:\Users\HMD\Desktop\rt" /s/e/h/y
+            addEnvPath("SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", "Path","c:\\soft1\\");
+           
+        }
+           break;  
+        case '3':
+            
+            break;
+        case '4':
             exit(0);
             break;
         default:
+        getchar();
             break;
         }
+        getchar();
+        getchar();
     }
     return 0;
 }
